99bfe055a6bbed92a0b136c709447fe19c4883ef
clever-cr <cleverumuhuza@gmail.com>
2023-05-03 12:05:40 +0200

    Add home page

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Footer from "../Layout/Footer"
+import NavBar from "../Layout/NavBar"
+import Hero from "../sections/Hero"
+
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
