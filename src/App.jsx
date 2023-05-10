e966e621470303cd9d1360e7361176dd54b30607
clever-cr <cleverumuhuza@gmail.com>
2023-05-10 17:41:22 +0200

    refactor codes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
-import Footer from "../components/Layout/Footer";
-import NavBar from "../components/Layout/NavBar";
 import Hero from "../components/sections/Hero";
 
 const Home = () => {
   return (
     <div>
-      <NavBar />
       <Hero />
-      <Footer />
     </div>
   );
 };
